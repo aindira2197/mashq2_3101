80250f37a2a7159c3930ce3247b17fa98fb9fdac
Azadova Indira <aindira2197@gmail.com>
2026-01-31 13:12:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# 8
+numbers = [int(input())] + numbers
+
+# 9
+letters[2:2] = ["X"]
+
+# 10
+odd[1:1] = [0]
+
+# 11
+x = input()
+print(letters.index(x))
+
+# 12
+nums = [5, 2, 8, 1, 4]
+print(nums.index(min(nums)))
+
+# 13
+print(letters.index("A"))
+
+# 14
+x = input()
+print(x in letters, letters.index(x) if x in letters else -1)
